b7940901cc3c4070d5b903cd6cc970d30415ecf2
xqy2006 <2601928946@qq.com>
2022-08-08 11:52:46 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,214 @@
+<template>
+<div class="markdown-body" style="margin-top: 15px;margin-inline-start: 15px;">
+    <h1>markdown编辑器</h1>
+</div>
+<div style="margin-top: 15px;margin-inline-start: 15px;">
+    <b>请输入导出文件名：</b>
+    <input v-model="filename" class="form-control input-sm" type="text" placeholder="导出文件名" aria-label="导出文件名" />
+</div>
+<div style="margin-top: 5px;">
+    <button v-if="!mddown" :disabled="filename==''" style="margin-inline-start: 15px;" class="btn btn-primary btn-sm" @click="to_md()">
+        导出markdown
+    </button>
+    <button v-if="mddown" style="margin-inline-start: 15px;" class="btn btn-primary btn-sm" aria-disabled="true"><span>Loading</span><span class="AnimatedEllipsis"></span></button>
+    <button v-if="!pdfdown" :disabled="filename==''" style="margin-inline-start: 15px;" class="btn btn-primary btn-sm" @click="to_pdf(20)">
+        导出pdf
+    </button>
+    <button v-if="pdfdown" style="margin-inline-start: 15px;" class="btn btn-primary btn-sm" aria-disabled="true"><span>Loading</span><span class="AnimatedEllipsis"></span></button>
+    <button v-if="!jpgdown" :disabled="filename==''" style="margin-inline-start: 15px;" class="btn btn-primary btn-sm" @click="to_jpg()">
+        导出jpg
+    </button>
+    <button v-if="jpgdown" style="margin-inline-start: 15px;" class="btn btn-primary btn-sm" aria-disabled="true"><span>Loading</span><span class="AnimatedEllipsis"></span></button>
+
+</div>
+<div class="markdown-body" style="margin-top: 15px;margin-inline-start: 15px;">
+    <h4>请输入markdown内容：</h4>
+</div>
+<span class="BtnGroup d-block mb-2" style="margin-top: 5px;margin-inline-start: 15px;white-space:nowrap;overflow:auto;">
+    <button class="BtnGroup-item btn btn-sm" @click="add1('**')">粗体</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add1('*')">斜体</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add1('~~')">删除线</button>
+    <button class="BtnGroup-item btn btn-sm" @click="title()">标题</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add1('\`')">单行代码</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add1('\n\`\`\`\n')">代码块</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add2('> ')">引用</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add2('- ')">无序列表</button>
+    <button class="BtnGroup-item btn btn-sm" @click="add2('------\n')">分割线</button>
+</span>
+<div>
+    <textarea style="margin-top: 5px;width: 100%;height: 250px" class="form-control" v-model="mdtext" ref="input"></textarea>
+</div>
+<div class="markdown-body" style="margin-top: 15px;margin-inline-start: 15px;">
+    <h4>预览：</h4>
+</div>
+<div style="margin-top: 15px;" class="markdown-body" ref="md">
+    <div v-html="get_md(mdtext)">
+    </div>
+</div>
+</template>
+
+<style lang="scss">
+@import "@primer/css/index.scss";
+@import "highlight.js/styles/github.css";
+
+html,
+body {
+    display: block;
+    margin: 0px;
+    margin-top: 0px;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    margin-left: 0px;
+}
+</style>
+
+<script>
+import hljs from 'highlight.js/lib/common';
+import MarkdownIt from 'markdown-it';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import FileSaver from 'file-saver'
+export default {
+    data() {
+        return {
+            mdtext: '',
+            filename: '',
+            pdfdown: false,
+            jpgdown: false,
+            mddown: false,
+        }
+    },
+
+    methods: {
+        to_pdf(length) {
+            this.pdfdown = true
+            html2canvas(this.$refs.md).then((canvas) => {
+                var contentWidth = canvas.width;
+                var contentHeight = canvas.height;
+
+                //一页pdf显示html页面生成的canvas高度;
+                var pageHeight = contentWidth / 592.28 * 841.89;
+                //未生成pdf的html页面高度
+                var leftHeight = contentHeight;
+                //页面偏移
+                var position = 0;
+                //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+                var imgWidth = 595.28 - length * 2;
+                var imgHeight = (595.28 - length * 2) / contentWidth * contentHeight;
+
+                var pageData = canvas.toDataURL('image/jpeg', 1.0);
+
+                var pdf = new jsPDF('', 'pt', 'a4');
+
+                //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+                //当内容未超过pdf一页显示的范围，无需分页
+                if (leftHeight < pageHeight) {
+                    pdf.addImage(pageData, 'JPEG', length, 0, imgWidth, imgHeight);
+                } else {
+                    while (leftHeight > 0) {
+                        pdf.addImage(pageData, 'JPEG', length, position, imgWidth, imgHeight)
+                        leftHeight -= pageHeight;
+                        position -= 841.89;
+                        //避免添加空白页
+                        if (leftHeight > 0) {
+                            pdf.addPage();
+                        }
+                    }
+                }
+                let blob = pdf.output('blob')
+                blob = blob.slice(0, blob.size, 'application/octet-stream')
+                FileSaver.saveAs(blob, this.filename + '.pdf')
+                //window.open(pdf.output("bloburl", { filename: "xqy-markdown.pdf" }));
+                this.pdfdown = false
+            });
+
+        },
+        to_jpg() {
+            this.jpgdown = true
+            html2canvas(this.$refs.md).then((canvas) => {
+                let blob = canvas.toDataURL('image/jpeg', 1.0);
+                FileSaver.saveAs(blob, this.filename + '.jpg')
+                this.jpgdown = false
+            });
+        },
+        to_md() {
+            this.mddown = true
+            var blob = new Blob([this.mdtext])
+            FileSaver.saveAs(blob, this.filename + '.md')
+            this.mddown = false
+
+        },
+        get_md(mds) {
+            const md = MarkdownIt({
+                breaks: true,
+                highlight: function (str, lang) {
+                    if (lang && hljs.getLanguage(lang)) {
+                        try {
+                            return hljs.highlight(str, {
+                                language: lang
+                            }).value;
+                        } catch (__) {}
+                    }
+
+                    return ''; // use external default escaping
+                }
+            });
+            const content = md.render(mds)
+            return content
+        },
+        add1(str1) {
+            const oldloc = this.$refs.input.selectionEnd
+            this.mdtext = this.mdtext.slice(0, this.$refs.input.selectionEnd) + str1 + str1 + this.mdtext.slice(this.$refs.input.selectionEnd)
+            this.$refs.input.focus();
+            this.$nextTick(() => {
+                this.$refs.input.selectionStart = oldloc + str1.length
+                this.$refs.input.selectionEnd = oldloc + str1.length
+            })
+        },
+        add2(str1) {
+            const oldloc = this.$refs.input.selectionEnd
+            //console.log(this.mdtext.slice(oldloc - 1, oldloc) == '\n' , this.mdtext.slice(0, oldloc - 1).search('\n') == -1 , this.mdtext.slice(0, oldloc - 1).search(str1) != -1)
+            if (this.mdtext.slice(oldloc - 1, oldloc) == '\n' || (this.mdtext.slice(0, oldloc - 1).search('\n') == -1 && this.mdtext.slice(0, oldloc).search(str1) == -1)) {
+                this.mdtext = this.mdtext.slice(0, this.$refs.input.selectionEnd) + str1 + this.mdtext.slice(this.$refs.input.selectionEnd)
+                var enter = 0
+            } else {
+                this.mdtext = this.mdtext.slice(0, this.$refs.input.selectionEnd) + "\n" + str1 + this.mdtext.slice(this.$refs.input.selectionEnd)
+                var enter = 1
+            }
+            this.$refs.input.focus();
+            this.$nextTick(() => {
+                this.$refs.input.selectionStart = oldloc + str1.length + enter
+                this.$refs.input.selectionEnd = oldloc + str1.length + enter
+            })
+        },
+        title() {
+            const oldloc = this.$refs.input.selectionEnd
+            const list = this.mdtext.split("\n")
+            console.log(list[list.length - 1].search('# ') != -1)
+            var text = ''
+            if (list[list.length - 1].slice(0, 1) == '#') {
+                for (var i = 0; i < list.length - 1; i++) {
+                    text += list[i] + '\n'
+                }
+                if (list[list.length - 1].search('###### ') != -1) {
+                    this.mdtext = text + '#' + list[list.length - 1].slice(6)
+                } else {
+                    this.mdtext = text + '#' + list[list.length - 1]
+                }
+                var enter = 0
+            } else {
+                for (var i = 0; i < list.length - 1; i++) {
+                    text += list[i] + '\n'
+                }
+                this.mdtext = text + '# ' + list[list.length - 1]
+                var enter = 1
+            }
+            this.$refs.input.focus();
+            this.$nextTick(() => {
+                this.$refs.input.selectionStart = oldloc + 1 + enter
+                this.$refs.input.selectionEnd = oldloc + 1 + enter
+            })
+        },
+    },
+}
+</script>
